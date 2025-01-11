535fd11bb2e52ea6f58bbcdc06f6e44d29c808c5
architchatur <architc@berkeley.edu>
2025-01-11 13:26:26 -0800

    CNNModel.py

--- CNNModel.py
+++ CNNModel.py
@@ -10,17 +10,20 @@ X_train = X_train / 255
 X_test = X_test / 255
 
 cnn = models.Sequential(
-    [layers.Conv2D(filters = 48, kernel_size = (3, 3), activation = 'relu', input_shape = (32, 32, 3)),
+    [layers.Conv2D(filters = 32, kernel_size = (9, 9), activation = 'relu', input_shape = (32, 32, 3), padding = 'same'),
      layers.MaxPooling2D((2, 2)),
      
-     layers.Conv2D(filters = 64, kernel_size = (3, 3), activation = 'relu'),
+     layers.Conv2D(filters = 48, kernel_size = (9, 9), activation = 'relu', padding = 'same'),
      layers.MaxPooling2D((2, 2)),
      
      layers.Flatten(), 
-     layers.Dense(100, activation = 'relu'), 
      layers.Dense(100, activation = 'relu'),
+     layers.Dense(100, activation = 'relu'),
+     layers.Dense(100, activation = 'relu'),
+     layers.Dense(50, activation = 'relu'),
+     layers.Dense(50, activation = 'relu'),
      layers.Dense(10, activation = 'softmax')])
 
-cnn.compile(optimizer = 'SGD', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+cnn.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
 
 cnn.fit(X_train, y_train, epochs = 10)
